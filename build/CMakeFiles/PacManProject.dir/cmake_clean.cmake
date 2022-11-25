61379860d63baeb8ec8ba073ed4bfcadb9ab0562
hendrixgg <hendrixg10@gmail.com>
2022-11-25 11:20:02 -0500

    added ability to run project in vscode

---
+++ build/CMakeFiles/PacManProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PacManProject.dir/apsc143project.cpp.obj"
+  "CMakeFiles/PacManProject.dir/apsc143project.cpp.obj.d"
+  "CMakeFiles/PacManProject.dir/colours.cpp.obj"
+  "CMakeFiles/PacManProject.dir/colours.cpp.obj.d"
+  "PacManProject.exe"
+  "PacManProject.exe.manifest"
+  "PacManProject.pdb"
+  "libPacManProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PacManProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
