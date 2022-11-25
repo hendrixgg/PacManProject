7e317a90913f88322c5f9c7e26051d7d9ae63325
hendrixgg <hendrixg10@gmail.com>
2022-11-24 23:37:25 -0500

    map is correct dimensions and now storing entity positions

--- apsc143project.cpp
+++ apsc143project.cpp
@@ -19,25 +19,40 @@
 #define DOWN 's'
 #define RIGHT 'd'
 #define ESC 27
-#define COLS 9
-#define ROWS 9
+#define COLS 11
+#define ROWS 11
 
-char ** initMap(const char *mapFilePath, int rows, int cols){
+void initGame(const char *mapFilePath, char ***map, int rows, int cols, int pacManPos[2], int ghostPos[2][2]){
     FILE* mapFile = fopen(mapFilePath, "r");
-    char **map = (char **)malloc(rows*sizeof(char*));
+    *map = (char **)malloc(rows*sizeof(char*));
+    int numGhosts = 0;
     for(int i = 0; i < rows; ++i) {
-        map[i] = (char *) malloc(cols * sizeof(char));
+        (*map)[i] = (char *) malloc(cols * sizeof(char));
         for(int j = 0; j < cols; ++j){
-            fscanf(mapFile, "%c ", &map[i][j]);
+            fscanf(mapFile, "%c ", &(*map)[i][j]);
+            if((*map)[i][j] == PACMAN) {
+                (*map)[i][j] = EMPTY;
+                pacManPos[0] = i, pacManPos[1] = j;
+            }else if((*map)[i][j] == GHOST){
+                (*map)[i][j] = DOT;
+                ghostPos[numGhosts][0] = i, ghostPos[numGhosts][1] = j, ++numGhosts;
+            }
         }
     }
-    return map;
 }
-void printMap(char **map, int rows, int cols){
+
+// TODO: add in colors
+void printMap(char **map, int rows, int cols, int pacManPos[2], int ghostPos[2][2]){
     for(int i = 0; i < rows; ++i){
         printf("%c ", map[i][0]);
         for(int j = 1; j < cols; ++j){
-            printf(" %c ", map[i][j]);
+            char c = map[i][j];
+            if(i == pacManPos[0] && j == pacManPos[1])
+                c = PACMAN;
+            for(int k = 0; k < 2; ++k)
+                if(i == ghostPos[k][0] && j == ghostPos[k][1])
+                    c = GHOST;
+            printf(" %c ", c);
         }
         puts("");
     }
@@ -53,20 +68,20 @@ int isWall(/*parameters*/){
 }
 
 int main() {
-    // load the map array (9 rows, 9 cols) of characters
-    char **map = initMap("map.txt", ROWS, COLS);
-    char input = 0;
-    // maybe save the location of
+    int pacManPos[2], ghostPos[2][2];
+    char **map, key = 0;
+    // load the map array (9 rows, 9 cols) of characters, and get initial PacMan and Ghost positions
+    initGame("map.txt", &map, ROWS, COLS, pacManPos, ghostPos);
+
     printf("press 'q' or esc to exit.\n");
     while(true){
         system("CLS");
-        printMap(map, ROWS, COLS);
+        printMap(map, ROWS, COLS, pacManPos, ghostPos);
         // input
-        if(input == ESC || input == 'q' || input == 'Q')
+        if(key == ESC || key == 'q' || key == 'Q')
             break;
-        printf("input: %d, %c\n", input, input);
-
-        // TODO: identify where the ghosts are (find them in the map or save their positions)
+        printf("input: %d, %c\n", key, key);
+        key = getch();
 
         // TODO: move ghosts
             // array for location of ghosts
@@ -77,8 +92,6 @@ int main() {
             // collect a pellet if PacMan lands on one
 
         // TODO: check if won/lost -> if yes: break the loop and print game over condition to user
-
-        input = getch();
     }
     printf("game over\n");
     printf("press any key to exit\n");
